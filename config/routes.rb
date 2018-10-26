cae94010a0fc9ce6201dd32020731b70bd27bbe2
Dick Davis <rdavis89@my.apsu.edu>
2018-10-25 21:45:16 -0500

    added javascript to normalize rails validations with bootstrap stylings; added partial to display errors on form submit; added partial to display flash messages; overrode clearance user controller and routes; added validations to user model; finished initial sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'public#index', as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: 'public#index'
+  end
+
+  resources :users, controller: 'users', only: [:create] do
+    resource :password, controller: 'clearance/passwords',
+                        only: %i[create edit update]
+  end
+
+  resources :passwords, controller: 'clearance/passwords', only: %i[create new]
+  resource :session, controller: 'sessions', only: %i[create new]
+
   root 'public#index'
 end
