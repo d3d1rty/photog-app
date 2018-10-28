fb93d0e0863711e51ae9a5624037786b0821118c
Dick Davis <rdavis89@my.apsu.edu>
2018-10-27 23:36:00 -0500

    finished model, views, and controller for package group resources

---
+++ app/models/package_group.rb
@@ -0,0 +1,12 @@
+##
+# = PackageGroup
+# Author::    Richard Davis
+# Copyright:: Copyright 2018-2019 Mushaka Solutions Inc.
+# License::   GNU Public License 3
+#
+# This controller provides validations and methods for the package groups model.
+#
+# Package group and package resources share a nested, one-to-many relationship.
+class PackageGroup < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 75 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_25_013520) do
+ActiveRecord::Schema.define(version: 2018_10_28_005605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "package_groups", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
